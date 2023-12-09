079cbe444ecb8bee5270bc9e0da2b0dec28c61aa
Walter Karl <walterkarl@hotmail.fr>
2023-12-10 00:27:25 +0100

    jour2 fini

--- day_2/star2.py
+++ day_2/star2.py
@@ -3,27 +3,14 @@ dataArray = file.read().split("\n")
 arraySize = len(dataArray)
 dataArray.remove(dataArray[arraySize - 1])
 
-
-# Part 1
-def isPossible(red, green, blue):
-    maxRed = 12
-    maxGreen = 13
-    maxBlue = 14
-    if maxRed >= red and maxGreen >= green and maxBlue >= blue:
-        return True
-    else:
-        return False
-
-somme = 0
+# Part 2
+res = 0
+gameMaxValues = []
 for line in dataArray:
-    # Séparer l'id de la  partie du reste:
-    id = line.split(":")[0].split(" ")[1]
-
     # Séparer les parties dans une liste :
     roundList = line.split(":")[1].split(";")
-
-    blue= 0
-    red =0
+    blue = 0
+    red = 0
     green = 0
     # Séparer les couleurs entres elles :
     for round in roundList:
@@ -34,17 +21,13 @@ for line in dataArray:
             colorList[i] = colorList[i].strip(',')
             # Récupèrer la valeur de chaque couleur :
             if colorList[i] == "red" and int(colorList[i - 1]) > red:
-                    red = int(colorList[i - 1])
+                red = int(colorList[i - 1])
             elif colorList[i] == "green" and int(colorList[i - 1]) > green:
-                    green = int(colorList[i - 1])
+                green = int(colorList[i - 1])
             elif colorList[i] == "blue" and int(colorList[i - 1]) > blue:
-                    blue = int(colorList[i - 1])
+                blue = int(colorList[i - 1])
+    gameMaxValues.append(int(red) * int(green) * int(blue))
+res += sum(gameMaxValues)
 
 
-    # Vérifier si la partie est possible :
-    if isPossible(red, green, blue):
-        # print('id : ' + id + ' est possible')
-        somme += int(id)
-    # else:
-        # print('id : ' + id + ' est impossible')
-print(somme)
+print(res)
